1f59d0cee39a318bd927109a512c06601f936064
Louis Cattell (Amith) <louisc211@gmail.com>
2019-07-04 20:12:57 +0100

    Update pipeline

--- build/Set-NugetSemVersion.ps1
+++ build/Set-NugetSemVersion.ps1
@@ -1,22 +1,103 @@
-$ver = "";
-$prereleaseTag = "beta";
-if($env:Build_Reason -eq "PullRequest" )
-{
-    $ver = "$env:GITVERSION_MAJORMINORPATCH-$prereleaseTag-$env:GITVERSION_PRERELEASENUMBER";
-} 
-elseif($env:BUILD_SOURCEBRANCHNAME -eq "master")
-{
-    $ver = "$env:GitVersion_NuGetVersionV2-$prereleaseTag"
+function Get-NugetVersion {
+    param (
+        [switch]$IsPreview,
+        [string]$PreReleaseTag = $null
+    )
+    $branchName = $env:BUILD_SOURCEBRANCHNAME;
+    $ver = "";
+
+    $preReleaseTag = Get-PrereleaseTag -PreReleaseTag $PreReleaseTag;
+    $baseVer = Get-BaseVersion -BranchName $branchName;
+    $ver = Get-FullVersion -BaseVersion $baseVer -BranchName $branchName -IsPreview $IsPreview -PreReleaseTag $preReleaseTag;
+
+    echo "##vso[task.setvariable variable=nugetVersion]$ver"
+    [Environment]::SetEnvironmentVariable("LC_NUGETVERSION", "$ver", "User")
+    Write-Host "Var is $ver";
+    Write-Host "Env is $env:LC_NUGETVERSION";
+    Write-Host "vso is $env:nugetVersion";
 }
-elseif ($env:BUILD_SOURCEBRANCHNAME -like "azure-pipelines-test*") {
-    $ver = "$env:GitVersion_NuGetVersionV2-$prereleaseTag-ReleasePipelineTest"
-}
-else 
+
+function Get-FullVersion 
 {
-    $ver = "$env:GitVersion_NuGetVersionV2-$prereleaseTag-$env:GITVERSION_PRERELEASENUMBER";
+    param (
+        [string]$BaseVersion,
+        [string]$BranchName,
+        [switch]$IsPreview,
+        [string]$PreReleaseTag = 'beta'
+    )
+    $ver = $BaseVersion;
+
+    if ($IsPreview) 
+    {
+        if ($BranchName -eq "master") 
+        {
+            $ver += "-$PreReleaseTag";
+        }
+
+        if ($BranchName -like "azure-pipelines-test*") 
+        {
+            $ver += "-$PreReleaseTag-ReleasePipelineTest";
+        }
+    }
+    else 
+    {
+      if ($BranchName -like "azure-pipelines-test*") 
+      {
+          $ver += "-ReleasePipelineTest" 
+      }  
+    }
+
+    $ver;
 }
-echo "##vso[task.setvariable variable=nugetVersion]$ver"
-[Environment]::SetEnvironmentVariable("LC_NUGETVERSION", "$ver", "User")
-Write-Host "Var is $ver";
-Write-Host "Env is $env:LC_NUGETVERSION";
-Write-Host "vso is $env:nugetVersion";
+
+function Get-AzurePipeliesTestVersion {
+    param (
+        [string]$BaseVersion,
+        [string]$BranchName,
+        [switch]$IsPreview,
+        [string]$PreReleaseTag
+    )
+    if ($IsPreview) 
+    {
+        if ($BranchName -like "azure-pipelines-test*" -and $IsPreview) 
+        {
+            $ver += "-$PreReleaseTag-ReleasePipelineTest";
+        }
+    }
+    elseif ($BranchName -like "azure-pipelines-test*") 
+    {
+        $ver += "-ReleasePipelineTest" 
+    }
+}
+
+function Get-BaseVersion {
+    param (
+        [string]$BranchName
+    )
+    $ver = "";
+
+    if ($BranchName -like "azure-pipelines-test*") 
+    {
+        $ver = "$env:GITVERSION_MAJORMINORPATCH"
+        #-$PreReleaseTag-ReleasePipelineTest
+    } 
+    else 
+    {
+        $ver = $env:GITVERSION_NUGETVERSIONV2;
+    }     
+}
+
+
+function Get-PrereleaseTag {
+    param (
+        [string]$PreReleaseTag = $null
+    )
+
+    if ($null -ne $env:GITVERSION_NUGETPRERELEASETAGV2 -or $env:GITVERSION_NUGETPRERELEASETAGV2 -ne "" ) 
+        {
+            $PreReleaseTag = $env:GITVERSION_NUGETPRERELEASETAGV2
+        }
+        if ($null -eq $PreReleaseTag) { $PreReleaseTag = "beta" }
+
+    $PreReleaseTag;
+}
